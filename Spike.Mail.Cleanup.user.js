c845769f7fbaa03b1e8c3f3aa916607c0ba994ce
PiPauwels <kruisje@gmail.com>
2024-12-25 17:07:49 +0100

    Update for Spike DOM changes

--- Spike.Mail.Cleanup.user.js
+++ Spike.Mail.Cleanup.user.js
@@ -9,12 +9,15 @@
 // @require      https://update.greasyfork.org/scripts/383527/701631/Wait_for_key_elements.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=spikenow.com
 // @grant        GM.getValue
+// @downloadURL https://update.greasyfork.org/scripts/487366/Spike%20quoted%20mail%20fix.user.js
+// @updateURL https://update.greasyfork.org/scripts/487366/Spike%20quoted%20mail%20fix.meta.js
 // ==/UserScript==
 
 const QUOTED_TEXT_REGEX_DUT = /\n?\s?Op\s(maandag|dinsdag|woensdag|donderdag|vrijdag|zaterdag|zondag|ma|di|wo|do|vr|za|zo)?\s?\d+\s.*(schreef|heeft).*(<|lt;)+.*@.*(>|gt;)+.*(geschreven)?(.*|\n)*/g;
-const DEFAULT_SIG_1 = /Enviado (de mi|de|desde) ((<|&lt;)a.*(>|&gt;)+)?.*/g;
+const DEFAULT_SIG_1 = /Enviado (de mi|de|desde) ((<|&lt;)a.*(>|&gt;)+)?.*(.*|\n)*/g;
 const DEFAULT_SIG_2 = /Verstuurd vanaf mijn iPhone(.*|\n)*/g;
 const TRAILING_NEWLINES = /(<br>|<br\/>|\s)+$/g;
+const TRAILING_EMPTY_DIVS = /(<div dir="auto"><br><\/div>|\n)*(<div><br><\/div>|<div id="ms-outlook-mobile-signature" dir="auto"><\/div>)/g;
 
 waitForKeyElements (".thread", addThreadListener, false);
 
@@ -42,5 +45,6 @@ function stripQuotedText (jNode)
     jNode.html( jNode.html().replace(QUOTED_TEXT_REGEX_DUT, "")
                             .replaceAll(DEFAULT_SIG_1,"")
                             .replaceAll(DEFAULT_SIG_2,"")
-                            .replaceAll(TRAILING_NEWLINES,""));
+                            .replaceAll(TRAILING_NEWLINES,"")
+                            .replaceAll(TRAILING_EMPTY_DIVS,""));
 }
